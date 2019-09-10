18f57f00c79b119b76adafa78ff0c90f05ca6044
Ckopei <cameron@kopels.com>
2019-09-10 19:00:58 -0400

    basic styling done. basic psuedocode done for javascript.

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+//to start off with, ill need variables for my questions,answers,incorrect answers, and correct answers.
+//for question and answers and pictures. i can use an objects inside a main array. maybe.
+//  var mainArray = [
+//     {
+//         question: 1,
+//         answers: ["1", "2", "3", "4,"],
+//         correct: 0, //----ill use this number to say if answer === answers[correct], then blah blah
+//         doggoImg: 0,//i have no idea how to use images inside of an Object.
+//     }
+
+//
+//loop through my array of question and answer objects.
+
+//append questions to the page
+    //$("parentelement").append("what-im-appending")
+//append image below question
+//append answers to the page, they function like buttons. maybe use all <p> tags as buttons.
+    //$("p").on("click", answerFunction)
+
+//if else's?
+//↓ ↓ ↓ ↓ ↓ ↓
+//event timer. in 15 seconds and nothing clicked, show "times up", show correct answer, wait 3secs, append next set.
+//if correct, append funny doge .gif to page, timer for a few seconds, append next set iterate correct variable
+//if incorrect, append sad doge.gif to page, timer for a few seconds, append next set. iterate incorrect variable.
+// ↑ ↑ ↑ ↑ ↑
+//if else's?
+
+//at the end of the quiz, show the number of correct answers, incorrect, show percentage, and button to restart.
+//whooooo boy, lots of jquery going on here. 
