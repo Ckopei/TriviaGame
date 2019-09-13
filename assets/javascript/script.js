3656bb6d4b89aa1826a2a7cae5a0f287eab40294
Ckopei <cameron@kopels.com>
2019-09-12 20:15:58 -0400

    fixed most of it, but reset doesnt work.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var incorrect = 0;
     let intervalId;
     let counter = 10;
+    let questionsAsked = 0;
     //reference "34-QuestionGame" classwork for q&a objects inside arrays. No idea how to work an image into it.
     //7 questions, answers, and true answer.
     var mainArray = [
@@ -58,51 +59,83 @@ $(document).ready(function () {
 
     function decrement() {
         counter--;
-        $("#timer").empty()
-        $("#timer").html(counter)
+        $(".time").empty()
+        $(".time").html("<h2 class='text-center'>Time left: " + counter + "</h2>")
         if (counter === 0) {
             $(".killMe").empty();
             $(".killMe").append("<img class='gifYes' src='assets//images/incorrect.gif'>")
-            $(".killMe").append("<h3> Sorry, You have to make a choice! </h3>")
-            $(".killMe").append("<h3 class='text-center'>The correct answer was: " + mainArray[i-1].answerTruth + "</h3>")
+            $(".killMe").append("<h3 class='text-center'> Sorry, You have to make a choice! </h3>")
+            $(".killMe").append("<h3 class='text-center'>The correct answer was: " + mainArray[i - 1].answerTruth + "</h3>")
             incorrect++;
             clearInterval(intervalId);
             appendStats();
-            counter = 10;
+            setTimeout(reset, 3000)
             // setTimeout(start, 5000);
-            
+
         }
     }
     //I made a separate function to append my questions and answers so that my loop is not dependent or changed by my main game logic.
     //recursion was ruining my iterators.
-    function appendQA() {
-        $(".killMe").empty();
-        //append the question.
-        $(".killMe").append("<h1>" + mainArray[i].question + "</h1>");
-        //append all answers of the object's array. use second iterator with for loop inside this timed function.
-        for (var j = 0; j < mainArray[i].answers.length; j++) {
-            $(".killMe").append("<p>" + mainArray[i].answers[j] + "</p")
-        }  
-        i++;
+    function reset() {
+
+        $(".tracker").empty()
+        i++
+        clearInterval(intervalId)
+        if (questionsAsked < mainArray.length) {
+            appendQA()
+        }
+        else {
+            $("#timer").empty()
+            $(".killMe").empty();
+            $(".killMe").append("<img class='gifYes' src='assets//images/finished.jpg'>")
+            $(".killMe").append("<h3 class='text-center'> You finished the quiz! </h3>")
+            $(".killMe").append("<button type='button' class='btn btn-danger'>Press me to restart!</button>")
+            correctAnswers = 0;
+            incorrectAnswers = 0;
+            unansweredQuestions = 0;
+            i = 0
+            setTimeout(appendQA, 2000)
+            // $("button").on("click", function () {
+            //     correctAnswers = 0;
+            //     incorrectAnswers = 0;
+            //     unansweredQuestions = 0;
+            //     i = 0;
+            //     reset()
+            // });
+        }
+
+        function appendQA() {
+            counter = 10
+            intervalId = setInterval(decrement, 1000)
+            $(".killMe").empty();
+            //append the question.
+            $(".killMe").append("<h1>" + mainArray[i - 1].question + "</h1>");
+            //append all answers of the object's array. use second iterator with for loop inside this timed function.
+            for (var j = 0; j < mainArray[i - 1].answers.length; j++) {
+                $(".killMe").append("<p>" + mainArray[i - 1].answers[j] + "</p")
+            }
+            questionsAsked++;
+        }
     }
+
     //this is my function to append the correct and incorrect values after each click.
     function appendStats() {
         $(".tracker").empty()
         $(".tracker").append("<h3> Correct: " + correct + "</h3>")
         $(".tracker").append("<h3> Incorrect: " + incorrect + "</h3>")
-        }
+    }
 
-    
+
     function start() {
-        
+
         //empty the button.
         //this is a set interval to run my decrement timer function every second. with Jorge's help, it's inside function(start)
         //to make it recursive. The on click starts it, and the if/else logic brings it back to start()
         clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000)
-        appendQA()
+
+        reset()
         //every time the function gets called, empty my correct and incorrect div, and append them updated.
-        
+
 
         $(document).on("click", "p", function () {
             // var value = $(this).text refers to the CLICK. In Jquery it knows "this" is an event listener in this case.
@@ -110,7 +143,7 @@ $(document).ready(function () {
 
             // clearInterval(intervalId)
 
-            if (value === mainArray[i-1].answerTruth) {
+            if (value === mainArray[i - 1].answerTruth) {
                 $("#timer").empty()
                 $(".killMe").empty();
                 console.log("great")
@@ -119,34 +152,37 @@ $(document).ready(function () {
                 correct++;
                 appendStats();
                 clearInterval(intervalId)
+                $(".time").empty()
+
                 console.log(i);
-                setTimeout(appendQA, 3000)
+                setTimeout(reset, 3000)
 
             }
 
-            else if (value !== mainArray[i-1].answerTruth) {
+            else if (value !== mainArray[i - 1].answerTruth) {
                 $("#timer").empty()
                 $(".killMe").empty();
                 console.log("bad")
                 $(".killMe").append("<img class='gifYes' src='assets//images/incorrect.gif'>")
                 $(".killMe").append("<h3 class='text-center'> Sorry, that's not correct! </h3>")
-                $(".killMe").append("<h3 class='text-center'>The correct answer was: " + mainArray[i-1].answerTruth + "</h3>")
+                $(".killMe").append("<h3 class='text-center'>The correct answer was: " + mainArray[i - 1].answerTruth + "</h3>")
                 incorrect++;
                 appendStats()
                 clearInterval(intervalId)
+                $(".time").empty()
                 console.log(i);
-                setTimeout(appendQA, 3000)
+                setTimeout(reset, 3000)
             }
-            
 
         });
 
         //To query dyanmic elements, always use document.element to find them. 
-        
+
 
     };
 
     $("button").on("click", function () {
+
         start()
     });
 
